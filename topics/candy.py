263fb77d5f5b4b0c60458fd2e2b21bfa5fd6d9b3
Samathingamajig <samathigamajig@gmail.com>
2021-01-02 00:02:10 -0600

    removed trailing whitespace in candy topic

--- topics/candy.py
+++ topics/candy.py
@@ -8,4 +8,4 @@ def candy(chatbot: Chatbot):
   else:
     chatbot.require_from_list("Do you prefer chocolate or fruit flavored candy?", None, "Chocolate", "Fruit")
     chatbot.require_string("What is your favorite candy?", None)
-    chatbot.send(f"My favorite kind of candy is a {chatbot.random_topics_list('candies')}")    
+    chatbot.send(f"My favorite kind of candy is a {chatbot.random_topics_list('candies')}")
