a1cc335bf39a46b15849269e48ad24b359579e8b
Samathingamajig <samathigamajig@gmail.com>
2021-01-03 17:49:08 -0600

    "your's" ➤ "yours"

--- topics/card_games.py
+++ topics/card_games.py
@@ -9,6 +9,6 @@ def card_games(chatbot: Chatbot):
     my_fav_game = chatbot.random_topics_list('card_games')
     chatbot.send(f"My favorite card game is {my_fav_game}.")
     same_fav_game = chatbot.require_boolean("Is that also your favorite card game?", None)
-    if not same_fav_game: chatbot.require_string("What's your's then?", None)
+    if not same_fav_game: chatbot.require_string("What's yours then?", None)
     chatbot.require_from_list("In general, do you prefer single player or multiplayer games?", None, "Single Player", "Multiplayer")
     chatbot.require_from_list("In general, do you prefer card games, board games, or video games?", None, "Card", "Board", "Video")
