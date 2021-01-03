d9cf781eda9fcfcdbd5acff6e285fef4b36e95fb
Samathingamajig <samathigamajig@gmail.com>
2021-01-03 02:36:05 -0600

    Added the games (not implemented into chatbot)

---
+++ games/blackjack.py
@@ -0,0 +1,148 @@
+import random
+
+def get_scores(mini_deck):
+  score = 0
+  aces = 0
+   # print("mini_deck", mini_deck)
+  for card in mini_deck:
+     # print("card", card)
+    if str.isdigit(card):
+      score += int(card)
+       # print("number")
+    elif card == "A":
+      aces += 1
+       # print("ace")
+    else:
+      score += 10
+       # print("face")
+     # print("score:", score)
+     # print("aces:", aces)
+  scores = [score]
+   # print("scores", scores)
+  for _ in range(aces):
+    temp_scores = scores[:]
+    scores = []
+    for score in temp_scores:
+      scores.append(score + 1)
+      scores.append(score + 11)
+     # print("scores", scores)
+  scores = [score for index, score in enumerate(scores) if score not in scores[index+1:]]
+   # print("final_scores", scores)
+  scores.sort()
+   # print("set_scores", scores)
+  return scores
+
+def print_table(human_deck, comp_deck, player_still_hitting=False):
+  human_scores = get_scores(human_deck)
+  if len(human_scores) > 1:
+    print("Your possible scores are: ", end="")
+  else:
+    print("Your score is:\t", end="")
+  print(" or ".join(map(str, human_scores)))
+  print(f"Your hand is:\t{' '.join(human_deck)}")
+  if player_still_hitting:
+    print(f"My score is at least: {' or '.join(map(str, get_scores([comp_deck[0]])))}")
+    print(f"My hand is:\t{comp_deck[0]} X")
+  else:
+    comp_scores = get_scores(comp_deck)
+    if len(comp_scores) > 1:
+      print("My possible scores are ", end="")
+    else:
+      print("My score is:\t", end="")
+    print(" or ".join(map(str, comp_scores)))
+    print(f"My hand is:\t{' '.join(comp_deck)}")
+
+deck = None
+human_deck = None
+comp_deck = None
+human_final_score = None
+comp_final_score = None
+human_5_cards = False
+comp_5_cards = False
+
+def blackjack():
+  global deck, human_deck, comp_deck, human_final_score, comp_final_score, human_5_cards, comp_5_cards
+  deck = "A 2 3 4 5 6 7 8 9 10 J Q K".split(" ") * 4
+  for _ in range(5):
+    random.shuffle(deck)
+  deck = iter(deck)
+  human_deck = [next(deck), next(deck)]
+  comp_deck = [next(deck), next(deck)]
+  human_final_score = None
+  comp_final_score = None
+  human_5_cards = False
+  comp_5_cards = False
+  while (player_still_hitting := True):
+    print()
+    print_table(human_deck, comp_deck, player_still_hitting)
+    print()
+    hit = None
+    while (awaiting_valid_response := True):
+      response = input("Do you want to hit or stand? ").lower()
+      if response[:3] == "hit":
+        hit = True
+        break
+      elif response[:5] == "stand":
+        hit = False
+        break
+      else:
+        print("Invalid response, please try again")
+    if hit:
+      human_deck.append(next(deck))
+    human_scores = get_scores(human_deck)
+    valid_human_scores = [score for score in human_scores if score <= 21]
+    invalid_human_scores = [score for score in human_scores if score not in valid_human_scores]
+    if len(valid_human_scores) == 0 or 21 in valid_human_scores or not hit or len(human_deck) == 5:
+      if len(valid_human_scores) > 0:
+        human_final_score = max(valid_human_scores)
+        if len(human_deck) == 5:
+          human_5_cards = True
+      else: human_final_score = min(invalid_human_scores)
+      break
+  
+  while (comp_still_hitting := True):
+    print()
+    print_table(human_deck, comp_deck)
+    print()
+    comp_scores = get_scores(comp_deck)
+    scores_17_through_21 = [score for score in comp_scores if 17 <= score <= 21]
+    scores_less_than_17 = [score for score in comp_scores if score < 17]
+    has_17_through_21 = len(scores_17_through_21) > 0
+    has_less_than_17 = len(scores_less_than_17) > 0
+    if human_final_score >= 21 or human_5_cards:
+      comp_final_score = max([*scores_17_through_21, *scores_less_than_17])
+      break
+    elif has_17_through_21:
+      print("Since I have a score between 17 and 21, I must stand.")
+      comp_final_score = max(scores_17_through_21)
+      break
+    elif not has_less_than_17:
+      invalid_comp_scores = [score for score in comp_scores if score > 21]
+      comp_final_score = min(invalid_comp_scores)
+      break
+    elif len(comp_deck) == 5:
+      comp_final_score = max(scores_less_than_17)
+      comp_5_cards = True
+      break
+    else:
+      print(f"Since I have a score of 16 or below, I must hit.")
+      comp_deck.append(next(deck))
+
+  
+  print("Final hands:")
+  print_table(human_deck, comp_deck)
+  print()
+  print("Final Scores:")
+  print(f"You:\t{human_final_score}")
+  print(f"Me:\t{comp_final_score}")
+  print()
+  if (human_final_score > comp_final_score or comp_final_score > 21) and human_final_score <= 21:
+    print("You win! Congratulations!")
+  elif (comp_final_score > human_final_score or human_final_score > 21) and comp_final_score <= 21:
+    print("I win! Good game!")
+  else:
+    print("We tied! Good game.") 
+  if human_5_cards or comp_5_cards: print("(This win was because of drawing 5 cards and not going over 21)")
+
+if __name__ == "__main__":
+  blackjack()
---
+++ games/tictactoe.py
@@ -0,0 +1,100 @@
+import math
+import random
+
+SIZE = 3
+HUMAN = "X"
+COMP = "O"
+board = None
+
+def check_for_win(board):
+  # Check for horizontal wins
+  for y in range(SIZE):
+    row = "".join(map(str,board[y]))
+    if row == HUMAN * 3:
+      return HUMAN
+    elif row == COMP * 3:
+      return COMP
+  
+  # Check for vertical wins
+  for x in range(SIZE):
+    column = "".join(map(str,[board[y][x] for y in range(SIZE)]))
+    if column == HUMAN * 3:
+      return HUMAN
+    elif column == COMP * 3:
+      return COMP
+  
+  # Check for diagonal wins
+  tl_br = "".join(map(str,[board[k][k] for k in range(SIZE)]))
+  if tl_br == HUMAN * 3:
+    return HUMAN
+  elif tl_br == COMP * 3:
+    return COMP
+  
+  tr_bl = "".join(map(str,[board[k][SIZE-k-1] for k in range(SIZE)]))
+  if tr_bl == HUMAN * 3:
+    return HUMAN
+  elif tr_bl == COMP * 3:
+    return COMP
+  
+  return None
+
+def num_to_row_and_column(num):
+  num -= 1
+  row = math.floor(num / SIZE)
+  column = num % 3
+  return row, column
+
+def print_board(board):
+  print((f"\n{'-'*11}\n").join(["|".join([f" {char} " for char in row]) for row in board]))
+
+def is_valid_space(board, num):
+  row, column = num_to_row_and_column(num)
+  return isinstance(board[row][column], int)
+
+def set_space(board, num, player):
+  row, column = num_to_row_and_column(num)
+  board[row][column] = player
+
+def tictactoe():
+  global board
+  board = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+  ]
+  player = HUMAN
+  while (winner := check_for_win(board)) is None:
+    print_board(board)
+    available_places = [board[row][column] for row in range(SIZE) for column in range(SIZE) if is_valid_space(board, 3*row + column + 1)]
+    if len(available_places) == 0:
+      winner = "Tie"
+      break
+    if player == HUMAN:
+      print("What position would you like to play in?")
+      while not (valid_position := False):
+        try:
+          response = int(input())
+        except ValueError:
+          print("Invalid input, please try again.")
+          continue
+        if response in range(1, 9 + 1) and is_valid_space(board, response):
+          set_space(board, response, player)
+          break
+        print("Invalid input, please try again.")
+    elif player == COMP:
+      place = random.choice(available_places)
+      print(f"I'm going to play at spot number {place}.")
+      set_space(board, place, COMP)
+    player = COMP if player == HUMAN else HUMAN
+  
+  print_board(board)
+
+  if winner == HUMAN:
+    print(f"Congratulations... you win.")
+  elif winner == COMP:
+    print(f"Yay! I won :D Good game.")
+  else:
+    print(f"This game resulted in a tie.")
+
+if __name__ == "__main__":
+  tictactoe()
