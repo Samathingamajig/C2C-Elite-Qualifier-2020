e4f90226b547168f386183a3f9478a4d8f6f2422
Samathingamajig <samathigamajig@gmail.com>
2021-01-02 18:34:02 -0600

    Fixed bug in pokemon topic

--- topics/pokemon.py
+++ topics/pokemon.py
@@ -8,7 +8,7 @@ def pokemon(chatbot: Chatbot):
     chatbot.send("Can't talk about stuff you don't like then...")
   else:
     games_played = chatbot.require_string("What Pokemon games have you played? (say \"None\" if you haven't played any)", None, lowercase=True)
-    if re.search(r"none", games_played) is None:
+    if re.search(r"none", games_played) is not None: # the search for "none" results in None if "none" is not found
       chatbot.send("I would highly recommend playing at least one Pokemon game.")
       chatbot.require_string("What's the reason you haven't played any Pokemon games?", None)
     else:
